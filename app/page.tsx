891e605492e6d8c76a8fa9e3e0bb2acd95db753e
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2024-04-19 21:16:28 -0700

    Added a paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Braxton Medeiros</h1>
+      <p>Hello! I am a first generation college student. Currently studying Software Engineering at Brigham Young University Idaho.</p>
     </main>
   );
 }
